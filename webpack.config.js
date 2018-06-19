2f5615f55a59ad4a02dc7a2a347d8be5ed092e56
jettairlines <lwanghe2@gmail.com>
2018-06-19 18:09:11 +0800

    webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+
 const webpack = require('webpack');
 
 module.exports = {
@@ -11,8 +12,17 @@ module.exports = {
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+        loader: 'file-loader',
+      },
     ],
   },
+
 
   plugins: process.argv.indexOf('-p') === -1 ? [] : [
     new webpack.optimize.UglifyJsPlugin({
